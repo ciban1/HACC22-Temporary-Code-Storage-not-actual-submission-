7773f357cb055dfe65db18f9243ddc6e321f3006
ciban1 <cibanez11173@gmail.com>
2022-08-21 23:40:01 -1000

    added new sentence

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
 name = input("Who are you?")
 if name == "Alice":
     print("I hardly know, sir.")
+    print("At least I know who I was when I got up this morning, but I think I must have been changed several times"/
+          "since.")
