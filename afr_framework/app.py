50fee487612d269783ba368b72e8f320cefcda50
Your Name <gargantuanshit@gmail.com>
2022-10-24 17:55:58 -1000

    working javascript and pyscript

--- afr_framework/app.py
+++ afr_framework/app.py
@@ -15,12 +15,6 @@ templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-
-# @app.get('/column_test', response_class=HTMLResponse)
-# def get_basic_form(request: Request):
-#     return templates.TemplateResponse("column_select_test.html", {"request": request})  # renders HTML form and returns to user (lets us see the form)
-
-# page 1
 @app.get('/page_1', response_class=HTMLResponse)
 def get_first_form(request: Request):
    return templates.TemplateResponse("page-1.html", {"request": request})  # renders HTML form and returns to user (lets us see the form)
@@ -31,23 +25,26 @@ async def post_first_form(request: Request, howmanyGraph: str = Form(...), csv_l
    print(howmanyGraph, 'graphs')
    dbName(csv_link)
    return templates.TemplateResponse("page-1.html", {"request": request})  # returns form
-
 #page 2
-# @app.get('/page_2', response_class=HTMLResponse)
-# def get_second_form(request: Request):
-#    return templates.TemplateResponse("page-2.html", {"request":request})
-#
-# @app.post('/page_2', response_class=HTMLResponse)
-# def post_second_form(request: Request, graphNum: str = Form(...), graphType: str = Form(...), checkbox_column1: bool = Form(False), checkbox_column2: bool = Form(False), checkbox_column3: bool = Form(False), checkbox_column4: bool = Form(False), checkbox_column5: bool = Form(False)):
-#    print('Graph Number', graphNum)
-#    print('Graph Type', graphType)
-#    print('CheckBoxColumn1', checkbox_column1)
-#    print('CheckBoxColumn2', checkbox_column2)
-#    print('CheckBoxColumn3', checkbox_column3)
-#    print('CheckBoxColumn4', checkbox_column4)
-#    print('CheckBoxColumn5', checkbox_column5)
-#    return templates.TemplateResponse('page-2.html', {'request': request})
+@app.get('/page_2', response_class=HTMLResponse)
+def get_second_form(request: Request):
+   return templates.TemplateResponse("page-2.html", {"request":request})
 
+@app.post('/page_2', response_class=HTMLResponse)
+def post_second_form(request: Request, graphNum: str = Form(...), graphType: str = Form(...), checkbox_column1: bool = Form(False), checkbox_column2: bool = Form(False), checkbox_column3: bool = Form(False), checkbox_column4: bool = Form(False), checkbox_column5: bool = Form(False)):
+   print('Graph Number', graphNum)
+   print('Graph Type', graphType)
+   print('CheckBoxColumn1', checkbox_column1)
+   print('CheckBoxColumn2', checkbox_column2)
+   print('CheckBoxColumn3', checkbox_column3)
+   print('CheckBoxColumn4', checkbox_column4)
+   print('CheckBoxColumn5', checkbox_column5)
+   return templates.TemplateResponse('page-2.html', {'request': request})
+   # async def post_second_form(request: Request, csv_link: str = Form(...)):
+   # return templates.TemplateResponse("page-1.html", {"request": request})  # returns form
 
 if __name__ == '__main__':
-    uvicorn.run(app)
+   uvicorn.run(app)
+
+
+
