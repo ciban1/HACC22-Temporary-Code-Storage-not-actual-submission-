7d3eb0283094491f2fc68b8f762db2651aeb7017
Miles.Hak <2421201177@k12.hi.us>
2022-10-26 20:40:50 -1000

    Miles code from actual AFR updated with VSC stuff and functioning checkbox form. Need to link to backend functions for column headers and also need to fix urls

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import uvicorn, os
+import uvicorn
 import re
 from fastapi import FastAPI, Request, Form, Depends, UploadFile, File
 from fastapi.responses import HTMLResponse, FileResponse
@@ -17,19 +17,24 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get('/page_1', response_class=HTMLResponse)
+@app.get_page2('/page_2', response_class=HTMLResponse)
+def get_bruh(request: Request):
+   return templates.TemplateResponse("page2-results.html", {"request": request})
+
+@app.get_page1('/page_1', response_class=HTMLResponse)
 def get_first_form(request: Request):
-    return templates.TemplateResponse("page-1.html", {
-        "request": request})  # renders HTML form and returns to user (lets us see the form)
+   return templates.TemplateResponse("page-1.html", {
+       "request": request})  # renders HTML form and returns to user (lets us see the form)
 
 
 @app.post('/page_1', response_class=HTMLResponse)
 async def post_first_form(request: Request, howmanyGraph: str = Form(...), csv_link: str = Form(...)):
-    print('CSV link:', csv_link)
-    print(howmanyGraph, 'graphs')
-    # dbName(csv_link)
-    columnDisplayPage2(howmanyGraph)
-    return templates.TemplateResponse("page-1.html", {"request": request})  # returns form
+   print('CSV link:', csv_link)
+   print(howmanyGraph, 'graphs')
+   # dbName(csv_link)
+   columnDisplayPage2(howmanyGraph)
+#    return templates.TemplateResponse("page2-results.html", {"request": request})  # returns form
+   get_page2()
 
 
 ##### page 2
@@ -37,48 +42,23 @@ async def post_first_form(request: Request, howmanyGraph: str = Form(...), csv_l
 '''jinja test'''
 
 
-@app.get('/bruh', response_class=HTMLResponse)
-def get_bruh(request: Request):
-    return templates.TemplateResponse("page2-results.html", {"request": request})
-
-
-@app.post('/bruh', response_class=HTMLResponse)
-def post_bruh(request: Request, column: list = Form(...)):  # array of selected columns; use position
-    print(column)
-    print(HTMLResponse)
-    return templates.TemplateResponse('page2-results.html', {'request': request})
-
-
-# page 2 stuff (above is test)
-''''''''''''''''''''''''''''''''''''''''''''''''
-
-
-@app.get('/page_2', response_class=HTMLResponse)
-def get_second_form(request: Request):
-    return templates.TemplateResponse("page-2.html", {"request": request})
-
 
 @app.post('/page_2', response_class=HTMLResponse)
-def post_second_form(request: Request, graphNum: str = Form(...), graphType: str = Form(...),
-                     checkbox_column1: bool = Form(False), checkbox_column2: bool = Form(False),
-                     checkbox_column3: bool = Form(False), checkbox_column4: bool = Form(False),
-                     checkbox_column5: bool = Form(False)):
-    print('Graph Number', graphNum)
-    print('Graph Type', graphType)
-    print('CheckBoxColumn1', checkbox_column1)
-    print('CheckBoxColumn2', checkbox_column2)
-    print('CheckBoxColumn3', checkbox_column3)
-    print('CheckBoxColumn4', checkbox_column4)
-    print('CheckBoxColumn5', checkbox_column5)
-    return templates.TemplateResponse('page-2.html', {'request': request})
-    # async def post_second_form(request: Request, csv_link: str = Form(...)):
-    # return templates.TemplateResponse("page-1.html", {"request": request})  # returns form
+def post_bruh(request: Request, column: list = Form(...), graph_type: list = Form(...)):  # array of selected columns; use position
+   print(column)
+   print(graph_type)
+   # print(HTMLResponse)
+   return templates.TemplateResponse('graph-page.html', {'request': request})
+
+
 
 
 @app.get('/graph', response_class=HTMLResponse)
 def get_graph(request: Request):
-    # graph_display_test()  ### IMAGE NEEDS TO BE CREATED FROM POST OF PREVIOUS, OTHERWISE WILL NOT BE MADE IN TIME FOR HTML REQUEST
-    return templates.TemplateResponse("graph-page.html", {"request": request})
+   # graph_display_test()  ### IMAGE NEEDS TO BE CREATED FROM POST OF PREVIOUS, OTHERWISE WILL NOT BE MADE IN TIME FOR HTML REQUEST
+   return templates.TemplateResponse("graph-page.html", {"request": request})
+
 
 if __name__ == '__main__':
-    uvicorn.run(app, port=int(os.environ.get("PORT", 8080)), host="0.0.0.0")
+   uvicorn.run(app)
+
