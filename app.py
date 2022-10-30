c154437e7b1cf06071e8b63410cf53e0d4de7796
ciban1 <cibanez11173@gmail.com>
2022-10-30 01:25:10 -1000

    most recent framework.  still need backend code, newest html

--- app.py
+++ app.py
@@ -117,45 +117,48 @@ async def get_third_form(request: Request, graphtotal: Union[int, None] = None, 
 async def post_third_form(request: Request, graphtotal: Union[int, None] = None, graph_type: Union[str, None] = None, hed1x: Optional[str] = Form(None), hed1y: Optional[str] = Form(None), hed2x: Optional[str] = Form(None), hed2y: Optional[str] = Form(None), hed3x: Optional[str] = Form(None), hed3y: Optional[str] = Form(None), hed4x: Optional[str] = Form(None), hed4y: Optional[str] = Form(None), hed5x: Optional[str] = Form(None), hed5y: Optional[str] = Form(None)):
    graph_type = ast.literal_eval(graph_type)
    if 1 <= graphtotal:
-      graph_one = dict(type=graph_type[0], x_axis_name=hed1x.replace("-x", ""), y_axis_name=hed1y.replace("-y", ""), settings=dict)
+      graph_one = dict(type=graph_type[0], x_axis_name=hed1x.replace("-x", ""), y_axis_name=hed1y.replace("-y", ""))
+      # settings=dict at the end
    else:
       graph_one = {}
    if 2 <= graphtotal:
-      graph_two = {"type": graph_type[1], "x_axis_name": hed2x.replace("-x", ""), "y_axis_name": hed2y.replace("-y", ""), "settings": dict}
+      graph_two = {"type": graph_type[1], "x_axis_name": hed2x.replace("-x", ""), "y_axis_name": hed2y.replace("-y", "")}
+      # "settings": dict at end
    else:
       graph_two = {}
    if 3 <= graphtotal:
-      graph_three = {"type": graph_type[2], "x_axis_name": hed3x.replace("-x", ""), "y_axis_name": hed3y.replace("-y", ""), "settings": dict}
+      graph_three = {"type": graph_type[2], "x_axis_name": hed3x.replace("-x", ""), "y_axis_name": hed3y.replace("-y", "")}
    else:
       graph_three = {}
    if 4 <= graphtotal:
-      graph_four = {"type": graph_type[3], "x_axis_name": hed4x.replace("-x", ""), "y_axis_name": hed4y.replace("-y", ""), "settings": dict}
+      graph_four = {"type": graph_type[3], "x_axis_name": hed4x.replace("-x", ""), "y_axis_name": hed4y.replace("-y", "")}
    else:
       graph_four = {}
    if 5 <= graphtotal:
-      graph_five = {"type": graph_type[4], "x_axis_name": hed5x.replace("-x", ""), "y_axis_name": hed5y.replace("-y", ""), "settings": dict}
+      graph_five = {"type": graph_type[4], "x_axis_name": hed5x.replace("-x", ""), "y_axis_name": hed5y.replace("-y", "")}
    else:
       graph_five = {}
    print("555", graph_one)
    return fastapi.responses.RedirectResponse(f'/page_4/?graphtotal={graphtotal}&graph_type={graph_type}&graph_one={graph_one}&graph_two={graph_two}&graph_three={graph_three}&graph_four={graph_four}&graph_five={graph_five}', status_code=status.HTTP_302_FOUND)
-
-
-### page 4
-
+ 
+###page 4
 @app.get('/page_4', response_class=HTMLResponse)
-async def get_first_form(request: Request, graphtotal: Union[int, None] = None, graph_type: Union[str, None] = None,):
-   # backend first generation of graph
-   # create_graph(values_dataframe, axis, graph_id: int, "Bar Graph", "Year", y_axis_name, settings,
-   #               x_axis_label, y_axis_label, title, x_data, y_data, show_legend, x_range, y_range,
-   #               x_axis_label_font_name, y_axis_label_font_name, title_font_name, x_axis_label_font_size,
-   #               y_axis_label_font_size, title_font_size)
-
-
+async def get_first_form(request: Request, graphtotal: Union[int, None] = None, graph_type: Union[str, None] = None, graph_one: Union[str, None] = None, graph_two: Union[str, None] = None, graph_three: Union[str, None] = None, graph_four: Union[str, None] = None, graph_five: Union[str, None] = None, ):
    columnDisplayPage4(graphtotal)
+   graph_one = ast.literal_eval(graph_one)
+   graph_two = ast.literal_eval(graph_two)
+   graph_three = ast.literal_eval(graph_three)
+   graph_four = ast.literal_eval(graph_four)
+   graph_five = ast.literal_eval(graph_five)
+   print(type(graph_one))
+   print(type(graph_two))
+   print(type(graph_three))
+   print(type(graph_four))
+   print(type(graph_five))
    return templates.TemplateResponse("page4-results.html", {"request": request})  # returns form
    
 @app.post('/page_4', response_class=HTMLResponse)
-def post_third_form(request: Request, graph1_name: str = Form(...), graph1_color: str = Form(...), graph2_name: Optional[str] = Form(None), graph2_color: Optional[str] = Form(None), graph3_name: Optional[str] = Form(None), graph3_color: Optional[str] = Form(None), graph4_name: Optional[str] = Form(None), graph4_color: Optional[str] = Form(None), graph5_name: Optional[str] = Form(None), graph5_color: Optional[str] = Form(None), graphtotal: Union[int, None] = None, graph_type: Union[str, None] = None,):
+def post_third_form(request: Request, graph1_name: str = Form(None), graph1_color: str = Form(None), graph2_name: Optional[str] = Form(None), graph2_color: Optional[str] = Form(None), graph3_name: Optional[str] = Form(None), graph3_color: Optional[str] = Form(None), graph4_name: Optional[str] = Form(None), graph4_color: Optional[str] = Form(None), graph5_name: Optional[str] = Form(None), graph5_color: Optional[str] = Form(None), graphtotal: Union[int, None] = None, graph_type: Union[str, None] = None,):
    print('graphname1', graph1_name)
    print('graphcolor1', graph1_color)
    print('graphname2', graph2_name)
@@ -167,6 +170,18 @@ def post_third_form(request: Request, graph1_name: str = Form(...), graph1_color
    print('graphname5', graph5_name)
    print('graphcolor5', graph5_color)
    return templates.TemplateResponse("page4-results.html", {"request": request})
-
+ 
+###page 5
+@app.get('/page_5', response_class=HTMLResponse)
+async def get_fifth_form(request: Request, graphtotal: Union[str, None] = None):
+    graphtotal = 2
+    columnDisplayPage5(graphtotal)
+    return templates.TemplateResponse("page5-results.html", {"request": request})  # returns form
+ 
+# html needs to allow scroll
+@app.post('/page_5', response_class=HTMLResponse)
+async def post_first_form(request: Request, graphtotal: int = Form(...), csv_link: str = Form(...)):
+    return fastapi.responses.RedirectResponse("page-5-results-html", {"request": request})
+ 
 if __name__ == '__main__':
    uvicorn.run(app)
